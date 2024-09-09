937ad0c18df2a33965a8ffeadca8f0f66e74a6d8
Soumik <soumiksaha4223@gmail.com>
2024-09-09 21:59:48 +0530

    Signup path added to root page and google icon added

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -144,13 +144,13 @@ export default function SignupPage() {
         </div>
         <div className="relative flex py-5 items-center">
           <div className="flex-grow border-t-2 border-gray-50"></div>
-          <span className="flex-shrink mx-4 text-gray-500">or</span>
+          <span className="flex-shrink mx-4 text-gray-300">or</span>
           <div className="flex-grow border-t-2 border-gray-50"></div>
         </div>
 
         <div className="flex justify-between space-x-4">
           <button className="flex items-center justify-center w-full p-2 bg-white border rounded-md hover:bg-gray-100 shadow-md">
-            <FaGoogle className="text-red-500" />
+          <img width="18" height="18" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo"/>
           </button>
           <button className="flex items-center justify-center w-full p-2 bg-white border rounded-md hover:bg-gray-100 shadow-md">
             <FaGithub className="text-black" />
